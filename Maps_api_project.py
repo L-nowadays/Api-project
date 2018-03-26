6e1313e45e1f216b28e316462f99b33bc62df765
Ivan <ivansablinofficial@gmail.com>
2018-03-26 15:14:33 +0300

    Offset coefficients, goals 1-3 are completed

--- Maps_api_project.py
+++ Maps_api_project.py
@@ -8,11 +8,13 @@ geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 max_map_size = 17
 min_map_size = 0
 # Offset for each map size
-offsets = [30, 30, 20, 15, 10, 3, 2, 1, 1, 0.5, 0.2, 0.1, 0.08, 0.008, 0.006, 0.0005, 0.0003, 0.0002]
+offsets = [[0, 0], [0, 0], [211, 82.9], [105.4, 62], [52.5, 37], [26.4, 19.65], [13.2, 9.65], [6.6, 4.865],
+           [3.3, 2.464], [1.65, 1.232], [0.825, 0.6165], [0.4125, 0.30825], [0.20625, 0.1542], [0.103125, 0.07521],
+           [0.0515625, 0.037605], [0.02578125, 0.0185], [0.012890625, 0.00940125], [0.0064453125, 0.004700625]]
 max_latitude = 180
 min_latitude = -180
-min_longitude = -89
-max_longitude = 89
+min_longitude = -85
+max_longitude = 85
 
 # Keys used to move or resize map
 control_keys = (pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN)
@@ -70,24 +72,24 @@ while running:
             if event.key == pygame.K_PAGEUP and map_size + 1 <= max_map_size:
                 map_size += 1
             if event.key in control_keys[2:]:
-                offset = offsets[map_size]
+                lat_offset, lon_offset = offsets[map_size]
                 if event.key == pygame.K_LEFT:
-                    latitude -= offset
+                    latitude -= lat_offset
                 if event.key == pygame.K_RIGHT:
-                    latitude += offset
+                    latitude += lat_offset
                 if event.key == pygame.K_UP:
-                    longitude += offset
+                    longitude += lon_offset
                 if event.key == pygame.K_DOWN:
-                    longitude -= offset
-                # Handle outside values for latitude and longitude
-                if latitude < min_latitude:
-                    latitude = max_latitude - offset
-                elif latitude > max_latitude:
-                    latitude = min_latitude + offset
+                    longitude -= lon_offset
+                # Handle border values
                 if longitude < min_longitude:
-                    longitude = max_longitude - offset
+                    longitude = min_longitude
                 elif longitude > max_longitude:
-                    longitude = min_longitude + offset
+                    longitude = max_longitude
+                if latitude < min_latitude:
+                    latitude = min_latitude
+                elif latitude > max_latitude:
+                    latitude = max_latitude
             print(longitude, latitude)
             # Each move action requires new map
             # Create and load new map
